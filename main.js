9df29b7a5c0c1e9aa15bce57f80b76294e51ac01
Tsyhankov Nikita Dmitrievich <nikita1996.tsyhankov@gmail.com>
2019-01-11 00:32:53 +0200

    Types test

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const tokenizerModule = require("./binding.js");
+import { napiTokenize } from "./binding.js";
 
 // const value = "42! +  85!!   +523.23E-10 - sin(ln(54156e22  +  90/43cos(5)))";
 const value = "42!+85!!+523.23E-10-sin(ln(54156e22+90/43cos(5)))";
-const resultArray = tokenizerModule.napiTokenize(value.length, value);
+const resultArray = napiTokenize(value.length, value);
 console.log(resultArray);
--- test.ts
+++ test.ts
@@ -1,18 +1,31 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
-
-import 'zone.js/dist/zone-testing';
 import {  Tokenizer } from '@scalc/core';
+
+import 'mocha';
 // import * as tkn from '@scalc/core';
 
 // First, initialize
-declare const require: any;
-declare const tokenizer: Tokenizer;
+import * as tokenizerModule from 'binding';
+declare const binding: any;
 
+
+import { expect } from 'chai';
+
+
+describe('Hello function', () => {
+    it('should return hello world', () => {
+
+
+        const value = "42!+85!!+523.23E-10-sin(ln(54156e22+90/43cos(5)))";
+        const resultArray = tokenizerModule.napiTokenize(value.length, value);
+
+        console.log(resultArray);
+        const result = 'Hello World!';
+        expect(result).to.equal('Hello World!');
+    });
+});
 
 const value = "42!+85!!+523.23E-10-sin(ln(54156e22+90/43cos(5)))";
-const resultArray = tokenizer.napiTokenize(value.length, value);
+const resultArray = tokenizerModule.napiTokenize(value.length, value);
 
-// Then we find all the tests.
-const context = require.context('./tests', true, /\.spec\.ts$/);
-// And load the modules.
-context.keys().map(context);
+console.log(resultArray);
